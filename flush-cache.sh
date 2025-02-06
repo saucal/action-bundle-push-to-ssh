809c07ae807a035a7f5f545926e5d6d9ea2f22b1
Kostas Seresiotis <kostas@saucal.com>
2025-02-06 17:56:10 +0200

    support auto mode that will run whatever cmd exists

--- flush-cache.sh
+++ flush-cache.sh
@@ -25,11 +25,15 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
+if [ "${INPUT_FLUSH_CACHE}" == 'false' ]; then
+    echo "Skipping cache flush" # We shouldn't be here if its false, but just in case.
+    exit 0
+fi
+
 echo "::group::Flushing cache"
 
-if [ "${INPUT_FLUSH_CACHE}" == 'auto' ] || [ "${INPUT_FLUSH_CACHE}" == 'default' ]; then
-    echo ::warning title=Flush cache::No Generic way to flush cache. Exiting.
-    exit 0;
+if [ "${INPUT_FLUSH_CACHE}" == 'default' ]; then
+    INPUT_FLUSH_CACHE="auto"
 fi
 
 exitcode=0
@@ -39,24 +43,29 @@ if [[ "${INPUT_FLUSH_CACHE}" == *"convesio"* ]]; then
     curl -s -f -I -X POST "${INPUT_FLUSH_CACHE_EXTRA_PARAMS}" || exitcode=$?
 fi
 
-if [[ "${INPUT_FLUSH_CACHE}" == *"kinsta"* ]]; then
+if [[ "${INPUT_FLUSH_CACHE}" == *"kinsta"* ]] || ([[ "${INPUT_FLUSH_CACHE}" == "auto" ]] && rcli cli has-command "kinsta cache purge"); then
     notice 'Flushing Kinsta cache'
     rcli kinsta cache purge || exitcode=$?
 fi
 
-if [[ "${INPUT_FLUSH_CACHE}" == *"wpe"* ]]; then
+if [[ "${INPUT_FLUSH_CACHE}" == *"wpe"* ]] || ([[ "${INPUT_FLUSH_CACHE}" == "auto" ]] && rcli cli has-command "cdn-cache flush"); then
     notice 'Flushing WPE cache'
     rcli cdn-cache flush || exitcode=$?
 fi
 
-if [[ "${INPUT_FLUSH_CACHE}" == *"objectcache"* ]]; then
-    notice 'Flushing object cache'
-    rcli cache flush || exitcode=$?
+if [[ "${INPUT_FLUSH_CACHE}" == *"wpe"* ]] || ([[ "${INPUT_FLUSH_CACHE}" == "auto" ]] && rcli cli has-command "nexcess-mapps cache flush"); then
+    notice 'Flushing NEXCESS cache'
+    rcli nexcess-mapps cache flush --all || exitcode=$?
 fi
 
-if [[ "${INPUT_FLUSH_CACHE}" == *"wprocket"* ]]; then
+if [[ "${INPUT_FLUSH_CACHE}" == *"wprocket"* ]] || ([[ "${INPUT_FLUSH_CACHE}" == "auto" ]] && rcli cli has-command "rocket clean"); then
     notice 'Flushing WP Rocket cache'
     rcli rocket clean --confirm || exitcode=$?
+fi
+
+if [[ "${INPUT_FLUSH_CACHE}" == *"objectcache"* ]] || [[ "${INPUT_FLUSH_CACHE}" == "auto" ]]; then
+    notice 'Flushing object cache'
+    rcli cache flush || exitcode=$?
 fi
 
 if [ $exitcode -ne 0 ]; then
